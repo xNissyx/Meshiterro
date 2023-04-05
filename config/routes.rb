7be615fc20014f8593bd2ef0628e5ff8a243ea02
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-04-05 06:44:07 +0000

    [finish] 4-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'homes/top'
+  get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 end
