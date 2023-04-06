fbf4cbe06c6a4ff685f2f495e2e32404260cc0f9
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-04-06 06:38:06 +0000

    [finish] 4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
          
   # アソシエーション
   has_many :post_images, dependent: :destroy
+  
+# has_one_attached :profile_imageという記述により、profile_imageという名前でActiveStorageで
+# プロフィール画像を保存できるように設定しました。
+  has_one_attached :profile_image
+  
+# メソッドに対して引数を設定し、引数に設定した値に画像のサイズを変換するようにしました。
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
