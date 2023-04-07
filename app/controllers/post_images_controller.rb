78fb5b3d201d930bde534c51c123bd29d38c2a3d
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-04-07 12:49:42 +0000

    [finish] 4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,7 +14,8 @@ class PostImagesController < ApplicationController
   end
   
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
+    # pageメソッドは、kaminariをインストールしたことで使用可能になったメソッドです
   end
 
   def show
